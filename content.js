6738c46e105361eeba97897e4b34c6dc3113e19a
Noah Giesel <noahgiesel@gmail.com>
2020-04-19 17:43:12 +0200

    manifest and js

---
+++ content.js
@@ -0,0 +1,16 @@
+/* 
+var section = document.getElementById("subscribe-button") 
+var button = document.createElement("paper-button");
+button.innerHTML = "Download"; 
+//section.appendChild(button)
+//console.log("working here exteis") 
+section.innerHTML="FIERo"
+
+document.body.appendChild(button) 
+section.appendChild(button) 
+
+
+//"matches": [ "*://*.youtube.com/*"],
+
+
+*/
